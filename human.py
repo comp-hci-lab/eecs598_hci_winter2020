7a986837db6cde7d7c4c3d126a9c78ac88e7be8f
Joshua Spalter <jspalt@umich.edu>
2019-07-02 14:40:21 -0400

    Made operator a visitor class Add properties of human Made motor ready to have push/press/grasp functionality based on device

--- human.py
+++ human.py
@@ -95,3 +95,84 @@ class Ears(BodyPart):
 	def send(send, auditory_operator):
 		pass
 
+class Memory(Human):
+
+	def __init__(self, newPiece=None):
+		if newPiece:
+			self.newPiece = newPiece
+
+	'''Operator is a visitor that is allowed or not to operate on the memory, acceptable types are perceptual or cognitive'''
+	def accept(self, operator):
+		pass
+
+	'''Send out operator from memory, acceptable types are cognitive or motor'''
+	def send(self, operator):
+		pass
+
+class LongTermMemory(Memory):
+
+	def __init__(self, newPiece=None):
+		super().__init__(newPiece)
+		self.LTM = []
+		if newPiece:
+			addToMemory(newPiece)
+
+	def addToMemory(self, newPiece):
+		self.LTM.append(newPiece)
+
+
+class ShortTermMemory(Memory):
+
+	def __init__(self, newPiece=None):
+		super().__init__(newPiece)
+		self.STM = []
+		if newPiece:
+			addToMemory(newPiece)
+
+	'''Operator is a visitor that is allowed or not to operate on the memory, acceptable types are perceptual or cognitive'''
+	def accept(self, operator):
+		pass
+
+	'''Send out operator from memory, acceptable types are cognitive or motor'''
+	def send(self, operator):
+		pass
+
+	def addToMemory(self, newPiece):
+		if self.newPiece in self.STM:
+				self.STM.remove(self.newPiece)
+				self.STM.insert(self.newPiece)
+		elif len(self.STM) > 3:
+			self.STM.append(self.newPiece)
+		else:
+			self.STM.pop()
+			self.STM.append(self.newPiece)
+
+class VisualShortTermMemory(ShortTermMemory):
+
+	def __init__(self):
+		super().__init__()
+
+	'''Modify accept to only take in visual operators'''
+	def accept(self, operator):
+		pass
+		
+
+class AuditoryShortTermMemory(ShortTermMemory):
+
+	def __init__(self):
+		super().__init__()
+
+	'''Modify accept to only take in auditory operators'''
+	def accept(self, operator):
+		pass
+
+
+class HapticShortTermMemory(ShortTermMemory):
+
+	def __init__(self):
+		super().__init__()
+
+	'''Modify accept to only take in haptic operators'''
+	def accept(self, operator):
+		pass
+		
--- operator.py
+++ operator.py
@@ -1,19 +1,33 @@
-class Operator():
+from abc import ABCMeta, abstractmethod
+
+NOTIMPLEMENTED = "Needs to be implemented"
+
+class OperatorElement():
 	#All operators use this as base
+	__metaclass__ = ABCMeta
+	
 	def __init__(self):
 		pass
 
+	@abstractmethod
+	def accept(self, visitor):
+		raise NotImplementedError(NOTIMPLEMENTED)
+
+	#Return a set of properties for what happens when you execute it (Time, cycle, etc)
+	#should return a snapshot of what is going on at that time along with how long it took
 	def execute(self):
-		#Return a set of properties for what happens when you execute it (Time, cycle, etc)
-		#should return a snapshot of what is going on at that time along with how long it took
+		raise NotImplementedError(NOTIMPLEMENTED)
 
 
-class Perceptual(Operator):
+class Perceptual(OperatorElement):
 
 	def __init__(self, storage):
 		#Initialize Perceptual values
 		#Need access to some sort of some storage
 		self.storage = storage
+
+	def accept(self, visitor):
+		visitor.visitPerceptual(self)
 		
 class Visual(Perceptual):
 
@@ -45,12 +59,17 @@ class Haptic(Perceptual):
 		pass
 
 
-class Cognitive(Operator):
+class Cognitive(OperatorElement):
 
 	def __init__(self):
 		#Have access to LTM/STM
 		self.long_term_memory = LongTermMemory()
 		self.short_term_memory = ShortTermMemory()
+
+
+	def accept(self, visitor):
+		visitor.visitCognitive(self)
+
 
 	'''Send information to short term memory'''
 	def execute(self):
@@ -61,10 +80,52 @@ class Cognitive(Operator):
 		pass
 
 
-class Motor(Operator):
+class Motor(OperatorElement):
 
 	def __init__(self):
 		pass
 
+	def accept(self, visitor):
+		visitor.visitMotor(self)
+
+	'''Make it so the correct action is taken based on the device held (i.e. button = push, key = press, NO_ITEM = grasp)'''
+	def execute(self, device):
+		pass
+
+
+class Operator(OperatorElement):
+	def __init__(self):
+		self.elements = [Perceptual(), Cognitive(), Motor()]
+
+	def accept(self, visitor):
+		for element in self.elements:
+			element.accept(visitor)
+
 	def execute(self):
 		pass
+
+class OperatorElementVisitor():
+	__metaclass__ = ABCMeta
+	@abstractmethod
+	def visitPerceptual(self, element):
+		raise NotImplementedError(NOTIMPLEMENTED)
+
+	@abstractmethod
+	def visitCognitive(self, element):
+		raise NotImplementedError(NOTIMPLEMENTED)
+
+	@abstractmethod
+	def visitMotor(self, element):
+		raise NotImplementedError(NOTIMPLEMENTED)
+
+
+class OperatorElementDoVisitor(OperatorElementVisitor):
+	def visitPerceptual(self, perceptual):
+		pass
+
+	def visitCognitive(self, cognitive):
+		pass
+
+	def visitMotor(self, motor):
+		pass
+
