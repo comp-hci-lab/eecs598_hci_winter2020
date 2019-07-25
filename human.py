8decaaf54685a78a259af965fc458cfa3b01c82e
Joshua Spalter <jspalt@umich.edu>
2019-07-25 16:12:50 -0400

    Fix accept handling of motor operators within finger, need to account for requests that cannot be fufilled still

--- human.py
+++ human.py
@@ -154,16 +154,41 @@ class Finger(BodyPart):
 		super().__init__(location_x, location_y, device)
 		self.location_x = location_x
 		self.location_y = location_y
-		self.device = devices
+		self.held_device = device
 		self.parent = None
 		self.children = None
+	
+	def accept(self, motor_operator):
+		'''Move finger'''	
+		if motor_operator not isinstance(motor_operator, MotorOperator):
+			#TODO Throw an exception
+			pass
 
-	'''Move finger'''
-	def accept(self, new_location_x, new_location_y):
-		self.location_x = new_location_x
-		self.location_y = new_location_y
+		if motor_operator.type == 'press':
+			'''Check to see if button is at location, if present press'''
+			if self.location_x != motor_operator.new_location_x or self.location_y != motor_operator.new_location_y:
+				'''Perform a move operator if locations aren't same'''
+				self.location_x = motor_operator.new_location_x
+				self.location_y = motor_operator.new_location_y
+			if self.held_device == None:
+				device = check_for_device(motor_operator.new_location_x, motor_operator.new_location_y)
+			if device == None or device.type != 'button':
+				'''If no device or non button device found at location'''
+				pass
+			device.press()
+		else if self.held_device == None and type == 'grasp':
+			if self.location_x != new_location_x and self.location_y != new_location_y:
+				'''Perform a move operator if locations aren't same'''
+				self.location_x = motor_operator.new_location_x
+				self.location_y = motor_operator.new_location_y
+			grasp()
+		else if type == 'move':
+			self.location_x = motor_operator.new_location_x
+			self.location_y = motor_operator.new_location_y
+			if self.held_device != None:
+				self.held_device.move(self.location_x, self.location_y, new_location_x, new_location_y)
 
-	def grasp(self, location_x, location_y):
+	def grasp(self):
 		self.device = check_for_device(location_x, location_y)
 
 class Eyes(BodyPart):
